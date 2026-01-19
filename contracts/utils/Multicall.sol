93dfc33f3cb55051b998ba1425fd0b64692193a2
RevelationOfTuring <michael_wangguanyi@163.com>
2026-01-19 17:15:08 +0800

    utils//Multicall.sol

--- contracts/utils/Multicall.sol
+++ contracts/utils/Multicall.sol
@@ -18,18 +18,29 @@ import {Context} from "./Context.sol";
  * to the subcall. This makes it safe to use with {ERC2771Context}. Contexts that don't affect the resolution of
  * {Context-_msgSender} are not propagated to subcalls.
  */
+// Multicall是一个非常经典且强大的工具合约。它的核心功能是：允许客户端在一个单一的外部交易中，批量调用当前合约的多个函数
+// 注：本合约完美兼容ERC2771，确保 _msgSender() 在批量调用中依然有效
 abstract contract Multicall is Context {
     /**
      * @dev Receives and executes a batch of function calls on this contract.
      * @custom:oz-upgrades-unsafe-allow-reachable delegatecall
      */
+
+    //
+    // 参数data：这是一个 bytes 数组，每个元素都是一段已经编码好的函数调用数据（即 abi.encodeWithSelector 生成的内容）
+    // 返回值 results：一个 bytes 数组，按顺序存储每个子调用执行后的返回结果
     function multicall(bytes[] calldata data) public virtual returns (bytes[] memory results) {
+        // 如果你使用了ERC2771Context（元交易/受信任的转发器），真实的发送者地址是被附加在msg.data末尾的。
+        // 如果msg.sender 不是 _msgSender()，说明这次调用是通过转发器来的。从当前msg.data尾部解析出真实发送者地址，即context
+        // 如果msg.sender 是_msgSender()，context为空
         bytes memory context = msg.sender == _msgSender()
             ? new bytes(0)
             : msg.data[msg.data.length - _contextSuffixLength():];
 
         results = new bytes[](data.length);
         for (uint256 i = 0; i < data.length; i++) {
+            // delegatecall本合约，从而能够触发合约内定义的其他 public 或 external 函数
+            // 将真实发送者地址（context）拼接到calldata尾部。这确保了在批量调用中，每一个子函数通过 _msgSender() 获取到的依然是最初发起交易的用户地址
             results[i] = Address.functionDelegateCall(address(this), bytes.concat(data[i], context));
         }
         return results;
